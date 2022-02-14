90766e4528f2cfa51b9543679808ad9e08f3b9b3
mooonzero <[mooonzero@github.com]>
2022-02-14 06:48:15 +0000

    1-2 commit

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-# a = 3
-# b = "like this"
-# c = True
-# bolean : true/false는 첫글자 대문자로 
-# print(a)
-# python에서는 대부분 snake 표기법을 사용
-a_string = "like this"
-a_number = 3
-a_float = 3.14
-a_boolean = False
-# None = nothing, only python has . 
-a_none = None
 
-print(type(a_boolean))
+# 1-2 tuples and dicts
+tup_days = ("Mon","Tue","Wed","Thu","Fri")
+print(type(tup_days)) # class tuples
 
-#1-1 Lists in Python
-
-#list = sequence type 중의 하나 
-#list , tuple 
-days = ["Mon","Tue","Wed","Thu","Fri"]
-print(days)
-#python standard library에 올라온 문법 check
-print("Mon" in days) # -> True
-print("Man" in days) # -> False
-
-print(days[3]) # -> Thu
-# -> java처럼 0부터 count
-
-print(len(days)) # -> length 의미
-#list는 mutable sequence라서 append(), reverse(), clear()등으로 변경 가능
-days.append("Sat") # 추가
-days.reverse()
-print(days)
+# name = "moon"
+# age = 32
+# Korean = True
+# fav_food = ["Coffee","Bab"]
+# 이런식으로 변수를 설정하는 건 큰 도움 X 
+#dictionary 설정하는 방법
+moon = {
+  "name" : "Moon",
+  "age" : 31,
+  "korean" : True,
+  "fav_food" : ["Coffee","Bab"]
+}
+print(moon)
+print(moon["fav_food"])
+moon["starving"] = True
+print(moon)
+#어떤 변수는 list,tuple,dict 안에 설정 가능
+anything = ("tada",True,7,None,False,"hey")

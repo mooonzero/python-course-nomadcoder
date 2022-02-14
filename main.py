059f5e422a3d2d7afe01e30d804dced6a587f4f7
mooonzero <>
2022-02-14 05:10:12 +0000

    commitgit add .

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-a = 3
-print(a)
+# a = 3
+# b = "like this"
+# c = True
+# bolean : true/false는 첫글자 대문자로 
+# print(a)
+# python에서는 대부분 snake 표기법을 사용
+a_string = "like this"
+a_number = 3
+a_float = 3.14
+a_boolean = False
+# None = nothing, only python has . 
+a_none = None
+
+print(type(a_boolean))

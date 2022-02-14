c79361d977a430507f08e00882cc3894f46c6e12
mooonzero <[ansyoung126@gamil.com]>
2022-02-14 05:46:03 +0000

    1.1 commit

--- main.py
+++ main.py
@@ -12,3 +12,22 @@ a_boolean = False
 a_none = None
 
 print(type(a_boolean))
+
+#1-1 Lists in Python
+
+#list = sequence type 중의 하나 
+#list , tuple 
+days = ["Mon","Tue","Wed","Thu","Fri"]
+print(days)
+#python standard library에 올라온 문법 check
+print("Mon" in days) # -> True
+print("Man" in days) # -> False
+
+print(days[3]) # -> Thu
+# -> java처럼 0부터 count
+
+print(len(days)) # -> length 의미
+#list는 mutable sequence라서 append(), reverse(), clear()등으로 변경 가능
+days.append("Sat") # 추가
+days.reverse()
+print(days)

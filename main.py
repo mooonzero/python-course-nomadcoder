d0c6e1a4fe93e809c80a9123587a5a4aa82850c5
moonzero <82999106+mooonzero@users.noreply.github.com>
2022-02-18 13:40:36 +0000

    2-2 3: 56

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-#1-12 Modules 
-# modules = 기능의 집합
-# module = java의 함수 같은 것 ,java 처럼 import 해줘야함
-# library에서 import해서 사용 가능한 다양한 module을 확인할 수 있음 
-#import math
-#print(math.fsum(-1.2))  fabs : 절대값 
-	#이런 식으로 import하면 모든 math module을 가져와서 비효율적임
+#2-2 navigating with Python
+# 1. URL로 접근 (indeed,stackOverFlow)
+# 2. 각 사이트의 페이지가 몇개인지 파악해야 함
+	# 2.1 각 페이지마다 들어와서 스크랩 해줘야하니까
+	# 2.2 indeed는 맞춤검색에 들어가서 한 페이지에 몇개의 게시글을 나타낼지 설정할 수 있음
 
-from math import ceil,fsum
-#from math import fsum as cute_sum
-	# as를 통해 import해 온 module의 이름을 변경할 수 있음
-#csv : common separated value
-
-print(ceil(1.2))
-print(fsum([1,2,3,4,5]))
-#from을 통해서 특정 module만 가지고 오면 math.sth가 아니라
-#sth으로만 사용 가능 .
-
-from calculator import plus as deohagi,minus 
-#다른 file에 있는 함수도 이런식으로 import 가능
-# 이런식으로 import해온 함수 중 특정 함수만 as로 변경 가능 
-
-print(deohagi(2,3),",",minus(1,2))
+#import requests를 사용하기 위해선 설치해줘야하지만 repl.it에선 다른 방법 사용
+#package -> requests 검색 Python HTTP for Human 찾아서 install

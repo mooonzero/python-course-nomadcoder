b08a23e81f6b7c017cf6555b1cb3e099572aa076
moonzero <82999106+mooonzero@users.noreply.github.com>
2022-02-25 14:30:16 +0000

    stackOverflow

---
+++ so.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+LIMIT = 10
+URL = f"https://stackoverflow.com/jobs?q=python"
+
+# step.1 get the page
+# step.2 make the request 
+# step.3 extract jobs

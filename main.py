cfde87593d045a3936d456d5b211231f6b39202a
moonzero <82999106+mooonzero@users.noreply.github.com>
2022-02-26 11:54:40 +0000

    2-9

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 #2-06_Extracting_Titles
-# import requests
-# from bs4 import BeautifulSoup
-# ==> indeed.py를 import 하니까 requests나 bs4는 따로 import 안해줘도 된다니 .. 신기
 from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
 
-indeed_jobs = get_indeed_jobs()
 
-print(indeed_jobs)
+#indeed_jobs = get_indeed_jobs()
+so_jobs = get_so_jobs()
+
 
 
 
 
 
 #print(max_indeed_page)
-
-# range(max_page)
-# #range(n) n크기의 배열을 만들어 줌 
-# for n in range(max_page):
-# 	print(f"start={n*50}")
--- so.py
+++ so.py
@@ -6,3 +6,14 @@ URL = f"https://stackoverflow.com/jobs?q=python"
 # step.1 get the page
 # step.2 make the request 
 # step.3 extract jobs
+
+def get_last_page():
+	result = requests.get(URL)
+	soup = BeautifulSoup(result.text,"html.parser")
+	pages = soup.find("div",{"class":"s-pagination"}).find_all("a")
+	
+	print(pages)
+
+def get_jobs():
+	last_page = get_last_page()
+	return []

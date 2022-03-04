6d5e97eb417e620d3fec02b6457cf11a49284b38
moonzero <82999106+mooonzero@users.noreply.github.com>
2022-03-04 11:58:16 +0000

    so - extract_job

--- so.py
+++ so.py
@@ -12,13 +12,15 @@ def get_last_page():
 	soup = BeautifulSoup(result.text,"html.parser")
 	pages = soup.find("div",{"class":"s-pagination"}).find_all("a")
 	last_page = pages[-2].get_text(strip=True)
-	#pages의 제일 마지막이 next btn이므로 그걸 빼고 출력해주려고 -2
 	return int(last_page)
-#last_page = pages[-2].string 으로 print 했을때는 None이 나옴 차이를 명확하게 알기!
-# .get_text(는 bs4의 함수 .. 
-#.get_text(strip=True) -> get_text안의 whitespace 제거
-#int(page)로 return 해준 이유 :
-	# range는 string이 아닌 int값만을 받기 ㄸㅐ문 ; 
+
+def extract_job(html):
+	title = html.find("h2",{"class":"mb4"}).find("a")["title"]
+	#unpacking value
+	company, location  = html.find("h3",{"class":"mb4"}).find_all("span",recursive = False)
+	# recursive = False ==> 하위 span tag는 접근 불가  
+	print(company.get_text(strip=True).strip("-"), location.get_text(strip=True))
+	return {'title':title}
 
 def extract_jobs(last_page):
 	jobs =[]
@@ -27,7 +29,10 @@ def extract_jobs(last_page):
 		soup = BeautifulSoup(result.text, "html.parser")
 		results = soup.find_all("div",{"class":"-job"})
 		for result in results:
-			print(result["data-jobid"])
+			job = extract_job(result)
+			jobs.append(job)
+	return jobs
+			
 def get_jobs():
 	last_page = get_last_page()
 	jobs = extract_jobs(last_page)
